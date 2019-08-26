a476a814de6ab7626c32ebc62a91bd99d84c2051
Emily G <aspentree79@gmail.com>
2019-08-26 12:08:48 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -14,16 +14,3 @@ end
 end
 return hash
 end
-
-
-
-# First need to create an empty hash.
-# Next, need to drill into hash and access the names so they can become the new keys
-# Iterate over (data) first by key/value parameters
-# Iterate over "value" parameter (named them "attributes, names")
-# Iterate over "names" parameter (named the singular "name")
-# Need to establish that, if there is currently no name hash, we need to create a new one.
-# Similarly, if there is no key array within the hash, we need to create one.
-# We build out and specify the contents of the new hash by specifying that names of each pigeon are now going to be the new keys
-# Each pigeons' specific "attributes" will be converted to a string and added to comprise each pigeon's individual hash.
-# Return the new hash
